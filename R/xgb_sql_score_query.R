6bc565a6f749352f2278dcbc85c39a67d62ad675
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2020-07-14 19:40:18 +0800

    add the option to output column or table for `xbl_sql_score_query`.

--- R/xgb_sql_score_query.R
+++ R/xgb_sql_score_query.R
@@ -5,33 +5,46 @@
 #' @param base_score Extract the scale value from \code{xgb_model$params$base_score}.
 #' @importFrom whisker whisker.render
 #' @export
-xgb_sql_score_query <- function(list_of_trees, features_table, feature_dict, base_score = 0.5, key_field = "id"){
-
+xgb_sql_score_query <-
+  function(list_of_trees,
+           output_type = c('table','column'),
+           features_table,
+           feature_dict,
+           base_score = 0.5,
+           key_field = "id") {
     # a swap list to render queries via whisker
-    swap <- list(
-        key_field = key_field,
-        features_table = features_table
-    )
+    swap <- list(key_field = key_field,
+                 features_table = features_table)
 
     # score_queries contains the score query for each tree in the list_of_trees
-    score_queries <- lapply(list_of_trees, function(tree){
-        xgb_tree_sql(tree, feature_dict)
+    score_queries <- lapply(list_of_trees, function(tree) {
+      xgb_tree_sql(tree, feature_dict)
     })
 
     # the query clause to sum the scores from each tree
-    swap$sum_of_scores <- paste(score_queries, collapse=' + ')
+    swap$sum_of_scores <- paste(score_queries, collapse = ' + ')
 
     swap$base_score <- base_score
 
     # score query that logit-transforms the sum_of_scores
-    q <- "
-    SELECT
-      {{{key_field}}},
-      1/(1+exp(-1*( {{{sum_of_scores}}} + ln({{{base_score}}}/(1-{{{base_score}}}))))) AS score
-    -- FROM `{{{features_table}}}`
-    FROM {{{features_table}}}
-  "
-    # the natural logarithm, use ln() in Impala and Hive
-    return(whisker::whisker.render(q,swap))
 
-}
+    if (output_type == 'column') {
+      q <- "
+        1/(1+exp(-1*( {{{sum_of_scores}}} + ln({{{base_score}}}/(1-{{{base_score}}}))))) AS score
+      "
+    }
+
+    if (output_type == 'table') {
+      q <- "
+      SELECT
+        {{{key_field}}},
+        1/(1+exp(-1*( {{{sum_of_scores}}} + ln({{{base_score}}}/(1-{{{base_score}}}))))) AS score
+      -- FROM `{{{features_table}}}`
+      FROM {{{features_table}}}
+      "
+    }
+
+    # the natural logarithm, use ln() in Impala and Hive
+    return(whisker::whisker.render(q, swap))
+
+  }
